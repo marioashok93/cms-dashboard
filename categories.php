2671b4c858c77e9b8072665f87f7f0272adce6bb
marioashok93 <marioashok93>
2018-05-07 22:28:52 +0530

    Finished part 1 of frontend for dashboard.php and category.php

---
+++ categories.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+	
+ 
+<div class="container-fluid">
+	
+<div class="row">
+	
+
+<div class="col-sm-2">
+	
+
+<h1>Ajay</h1>
+
+
+<ul class="nav nav-pills nav-stacked">
+	
+
+<li><a href="dashboard.php">Dashboard</a></li>
+
+
+<li><a href="#">Add new Post</a></li>
+
+
+<li class="active"><a href="categories.php">Categories</a></li>
+
+<li><a href="#">Logout</a></li>
+
+<li><a href="#">Logout</a></li>
+
+<li><a href="#">Logout</a></li>
+
+
+
+</ul>
+
+
+</div>
+
+
+<div class="col-sm-10">
+	
+<h4>Add a Category</h4>
+ 
+
+
+
+</div>
+
+
+
+</div>
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>	
+</body>
+</html>
--- dashboard.php
+++ dashboard.php
@@ -7,7 +7,101 @@
 </head>
 <body>
 	
- <p>Hello</p>
+ 
+<div class="container-fluid">
+	
+<div class="row">
+	
+
+<div class="col-sm-2">
+	
+
+<h1>Ajay</h1>
+
+
+<ul class="nav nav-pills nav-stacked">
+	
+
+<li class="active"><a href="dashboard.php">Dashboard</a></li>
+
+
+<li><a href="#">Add new Post</a></li>
+
+
+<li><a href="categories.php">Categories</a></li>
+
+<li><a href="#">Logout</a></li>
+
+<li><a href="#">Logout</a></li>
+
+<li><a href="#">Logout</a></li>
+
+
+
+</ul>
+
+
+</div>
+
+
+<div class="col-sm-10">
+	
+<h4>About us</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsum quo dolore praesentium ipsa deleniti id, error architecto, quos illo perspiciatis hic. Deleniti architecto exercitationem, facilis repellat autem ipsam, voluptas?</p>
+
+<h4>About us</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsum quo dolore praesentium ipsa deleniti id, error architecto, quos illo perspiciatis hic. Deleniti architecto exercitationem, facilis repellat autem ipsam, voluptas?</p>
+
+
+<h4>About us</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsum quo dolore praesentium ipsa deleniti id, error architecto, quos illo perspiciatis hic. Deleniti architecto exercitationem, facilis repellat autem ipsam, voluptas?</p>
+
+
+<h4>About us</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsum quo dolore praesentium ipsa deleniti id, error architecto, quos illo perspiciatis hic. Deleniti architecto exercitationem, facilis repellat autem ipsam, voluptas?</p>
+
+
+<h4>About us</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsum quo dolore praesentium ipsa deleniti id, error architecto, quos illo perspiciatis hic. Deleniti architecto exercitationem, facilis repellat autem ipsam, voluptas?</p>
+
+
+<h4>About us</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsum quo dolore praesentium ipsa deleniti id, error architecto, quos illo perspiciatis hic. Deleniti architecto exercitationem, facilis repellat autem ipsam, voluptas?</p>
+
+
+<h4>About us</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsum quo dolore praesentium ipsa deleniti id, error architecto, quos illo perspiciatis hic. Deleniti architecto exercitationem, facilis repellat autem ipsam, voluptas?</p>
+
+
+<h4>About us</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsum quo dolore praesentium ipsa deleniti id, error architecto, quos illo perspiciatis hic. Deleniti architecto exercitationem, facilis repellat autem ipsam, voluptas?</p>
+
+
+<h4>About us</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsum quo dolore praesentium ipsa deleniti id, error architecto, quos illo perspiciatis hic. Deleniti architecto exercitationem, facilis repellat autem ipsam, voluptas?</p>
+
+
+<h4>About us</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsum quo dolore praesentium ipsa deleniti id, error architecto, quos illo perspiciatis hic. Deleniti architecto exercitationem, facilis repellat autem ipsam, voluptas?</p>
+
+
+</div>
+
+
+
+</div>
+
+
+
+
+
+
+
+</div>
+
+
+
+
 
 <script src="assets/js/bootstrap.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>	
