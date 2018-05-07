16f25d6840cfaaaab8565c5a2a95858cc1731f48
marioashok93 <marioashok93>
2018-05-07 22:20:09 +0530

    Added the required Bootstrap classes

---
+++ dashboard.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+	
+ <p>Hello</p>
+
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>	
+</body>
+</html>
